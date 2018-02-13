3064c7ee94eec31cc179c708b8d468378a68a8e0
Amr ELghadban <karmaadam.team@gmail.com>
2018-02-13 18:11:32 +0200

    init project code files

---
+++ CityCountryList/CityCountryListTests/CityCountryListTests.swift
@@ -0,0 +1,36 @@
+//
+//  CityCountryListTests.swift
+//  CityCountryListTests
+//
+//  Created by Amr ELghadban on 2/13/18.
+//  Copyright © 2018 Amr ELghadban. All rights reserved.
+//
+
+import XCTest
+@testable import CityCountryList
+
+class CityCountryListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
